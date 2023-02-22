85483ebc40fd2e4a999194008c1b66b96bc358e0
Furkan Akkurt <71407287+furkanakkurt1335@users.noreply.github.com>
2023-02-22 20:45:30 +0300

    fix problem of not including every other course

--- get_courses.py
+++ get_courses.py
@@ -16,15 +16,27 @@ dept = args.dept.upper()
 dept_url = dept.replace(' ', '+')
 term = 1
 
-filepath = 'course_d.json'
+filepath = 'course_l.json'
 if os.path.exists(filepath):
     with open(filepath, 'r', encoding='utf-8') as f:
-        course_d = json.load(f)
+        course_l = json.load(f)
 else:
-    course_d = dict()
+    course_l = list()
+
+def get_d(tr):
+    td_l = tr.find_all('td')
+    course_code = td_l[code_index].text.replace(' ', '').strip()
+    course_name = td_l[name_index].text.strip()
+    instructor = td_l[instructor_index].text.strip()
+    d_t = {'course_code': course_code, 'course_name': course_name, 'instructor': instructor, 'dept': dept, 'semester': semester}
+    if d_t in course_l or course_code == '': # already exists or no course code
+        return -1
+    else:
+        return d_t
 
 while year < 2023:
     r = requests.get(url.format(year=year, year_plus_1=year_plus_1, short_name=short_name, term=term, dept=dept_url))
+    semester = f'{year}/{year_plus_1}-{term}'
     content_t = r.content
     soup = BeautifulSoup(content_t, 'html.parser')
     tr_title_l = soup.find_all('tr', {'class': 'schtitle'})
@@ -38,20 +50,21 @@ while year < 2023:
                 name_index = i
             elif 'instr' in td_title.text.lower():
                 instructor_index = i
-    tr_l = soup.find_all('tr', {'class': 'schtd2'})
-    for tr in tr_l:
-        td_l = tr.find_all('td')
-        course_code = td_l[code_index].text.replace(' ', '').strip()
-        course_name = td_l[name_index].text.strip()
-        instructor = td_l[instructor_index].text.strip()
-        print(course_code, course_name, instructor, dept, f'{year}/{year_plus_1}-{term}')
-        key_t = f'{course_code} - {year} - {term}'
-        if key_t in course_d.keys():
-            print('Already exists', key_t)
-        elif course_code == '':
-            print('No course code')
-        else:
-            course_d[f'{course_code} - {year} - {term}'] = {'course_code': course_code, 'course_name': course_name, 'instructor': instructor, 'dept': dept, 'semester': f'{year}/{year_plus_1}-{term}'}
+    tr_grey_l = soup.find_all('tr', {'class': 'schtd2'})
+    tr_white_l = soup.find_all('tr', {'class': 'schtd'})
+    course_cnt = len(tr_grey_l) + len(tr_white_l)
+    grey_cnt, white_cnt = 0, 0
+    for i in range(max(len(tr_grey_l), len(tr_white_l))):
+        if i < len(tr_grey_l):
+            d_t = get_d(tr_grey_l[i])
+            grey_cnt += 1
+            if d_t != -1:
+                course_l.append(d_t)
+        if i < len(tr_white_l):
+            d_t = get_d(tr_white_l[i])
+            white_cnt += 1
+            if d_t != -1:
+                course_l.append(d_t)
     if term == 3:
         term = 1
         year += 1
@@ -59,5 +72,5 @@ while year < 2023:
     else:
         term += 1
 
-with open('course_d.json', 'w', encoding='utf-8') as f:
-    json.dump(course_d, f, indent=4, ensure_ascii=False)
+with open(filepath, 'w', encoding='utf-8') as f:
+    json.dump(course_l, f, indent=4, ensure_ascii=False)
